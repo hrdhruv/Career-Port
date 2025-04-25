84b5cd6da061d627a293c74b1e7ddef0e9148ae2
hrdhruv <122365238+hrdhruv@users.noreply.github.com>
2025-04-25 18:56:19 +0530

    Delete src directory

--- src/main/java/comjobport/joblist/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package comjobport.joblist.repository;
-
-import comjobport.joblist.model.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository<Post,String>
-{
-
-}
